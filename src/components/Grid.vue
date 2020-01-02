714f2844fe9e5098a99a3f47aba67885c7a0eaf3
Massimo Biagioli <biagiolimassimo@gmail.com>
2020-01-02 07:35:50 +0100

    Grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -4,8 +4,14 @@
       <span>Loading ...</span>
     </div>
     <div v-else>
-      <span>Todos: </span>
-      <div><pre>{{todos}}</pre></div>
+      <v-data-table
+        :headers="headers"
+        :items="todos"
+        :options.sync="options"
+        :server-items-length="totalTodos"
+        :loading="isLoading"
+        class="elevation-1"
+      ></v-data-table>
     </div>
   </div>
 </template>
@@ -17,8 +23,36 @@ export default {
   name: "Grid",
   data: () => ({
     isLoading: true,
-    todos: {}
+    todos: {},
+    totalTodos: 1000,
+    headers: [
+      {
+        text: 'Id',
+        align: 'center',
+        sortable: true,
+        value: 'id',
+      },
+      { 
+        text: 'Title', 
+        align: 'left',
+        sortable: true,
+        value: 'title' 
+      }
+    ],
+    options: {}
   }),
+  watch: {
+    options: {
+      async handler () {
+        //const { sortBy, sortDesc, page, itemsPerPage } = this.options;
+        //let repo = new TodoRepository();
+        //this.isLoading = true;
+        //this.todos = await repo.getTodos();
+        //this.isLoading = false;
+      },
+      deep: true,
+    },
+  },
   async mounted() {
     let repo = new TodoRepository();
     this.todos = await repo.getTodos();
